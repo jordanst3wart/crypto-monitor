af4e26d6f851d3a3857f7578c26ccff44f1b2f2e
Jordan <jordanstewart2428@gmail.com>
2019-05-07 23:48:19 +1000

    simplify

--- main/main.go
+++ main/main.go
@@ -135,106 +135,10 @@ func main() {
 	//DEBUG := true
 	chRates := make(chan ExchangeRates)
 	go currencyExchangeRates(chRates)
-	//groupList := []structs.CryptoDTO{}
 	ch := make(chan structs.CryptoDTO)
 
-	foo := []startData{{"CoinfloorTickerAndBitstamp", []Four{
-		{"Coinfloor_BTC", "https://webapi.coinfloor.co.uk:8090/bist/XBT/GBP/ticker/", "GBP", "BTC"},
-		{"Coinfloor_ETH","https://webapi.coinfloor.co.uk:8090/bist/ETH/GBP/ticker/", "GBP", "ETH"},
-		{"Coinfloor_BCH","https://webapi.coinfloor.co.uk:8090/bist/BCH/GBP/ticker/", "GBP", "BCH"},
-		{"Bitstamp_BTC","https://www.bitstamp.net/api/v2/ticker/btcusd/", "USD", "BTC"},
-		{"Bitstamp_XRP","https://www.bitstamp.net/api/v2/ticker/xrpusd/", "USD", "XRP"},
-		{"Bitstamp_LTC","https://www.bitstamp.net/api/v2/ticker/ltcusd/", "USD", "LTC"},
-		{"Bitstamp_ETH","https://www.bitstamp.net/api/v2/ticker/ethusd/", "USD", "ETH"},
-		{"Bitstamp_BCH","https://www.bitstamp.net/api/v2/ticker/bchusd/", "USD", "BCH"}}},
-		startData{"IndepentReserve",[]Four{
-		{"IndependentReserve_BTC", "https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xbt&secondaryCurrencyCode=aud", "AUD","BTC"},
-		{"IndependentReserve_ETH","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=eth&secondaryCurrencyCode=aud", "AUD", "ETH"},
-		{"IndependentReserve_BCH","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=bch&secondaryCurrencyCode=aud", "AUD", "BCH"},
-		{"IndependentReserve_XRP","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xrp&secondaryCurrencyCode=aud", "AUD", "XRP"},
-		{"IndependentReserve_LTC","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=ltc&secondaryCurrencyCode=aud", "AUD", "LTC"}}},
-		startData{"GeminiTickerBTC", []Four{
-		{"GEMINI_BTC", "https://api.gemini.com/v1/pubticker/btcusd", "USD", "BTC"}}},
-		startData{"GeminiTickerETH",[]Four{
-		{"GEMINI_ETH", "https://api.gemini.com/v1/pubticker/ethusd", "USD", "ETH"}}},
-		startData{"BTCMarket", []Four{
-		{"BTCMarket_AUD_BTC", "https://api.btcmarkets.net/market/BTC/AUD/tick", "AUD", "BTC"},
-		{"BTCMarket_AUD_ETH", "https://api.btcmarkets.net/market/ETH/AUD/tick", "AUD", "ETH"},
-		{"BTCMarket_AUD_BCH","https://api.btcmarkets.net/market/BCHABC/AUD/tick", "AUD", "BCH"},
-		{"BTCMarket_AUD_XRP", "https://api.btcmarkets.net/market/XRP/AUD/tick", "AUD", "XRP"},
-		{"BTCMarket_AUD_LTC","https://api.btcmarkets.net/market/LTC/AUD/tick", "AUD", "LTC"}}},
-		startData{"ACXTicker",[]Four{
-		{"ACX_AUD_BTC", "https://acx.io:443/api/v2/tickers/btcaud.json", "AUD", "BTC"},
-		{"ACX_AUD_ETH", "https://acx.io:443/api/v2/tickers/ethaud.json", "AUD", "ETH"},
-		{"ACX_AUD_BCH","https://acx.io:443/api/v2/tickers/bchaud.json", "AUD", "BCH"},
-		{"ACX_AUD_XRP", "https://acx.io:443/api/v2/tickers/ltcaud.json", "AUD", "XRP"},
-		{"ACX_AUD_LTC","https://acx.io:443/api/v2/tickers/xrpaud.json","AUD", "LTC"}}},
-		startData{"Coinjar",[]Four{
-		{"Coinjar_AUD_BTC", "https://data.exchange.coinjar.com/products/BTCAUD/ticker", "AUD", "BTC"},
-		{"Coinjar_AUD_ETH", "https://data.exchange.coinjar.com/products/ETHAUD/ticker", "AUD", "ETH"},
-		{"Coinjar_AUD_XRP","https://data.exchange.coinjar.com/products/XRPAUD/ticker","AUD", "XRP"},
-		{"Coinjar_AUD_LTC", "https://data.exchange.coinjar.com/products/LTCAUD/ticker", "AUD", "LTC"}}}}
-	/*
-	urlList := []Four{
-		{"Coinfloor_BTC", "https://webapi.coinfloor.co.uk:8090/bist/XBT/GBP/ticker/", "GBP", "BTC"},
-		{"Coinfloor_ETH","https://webapi.coinfloor.co.uk:8090/bist/ETH/GBP/ticker/", "GBP", "ETH"},
-		{"Coinfloor_BCH","https://webapi.coinfloor.co.uk:8090/bist/BCH/GBP/ticker/", "GBP", "BCH"},
-		{"Bitstamp_BTC","https://www.bitstamp.net/api/v2/ticker/btcusd/", "USD", "BTC"},
-		{"Bitstamp_XRP","https://www.bitstamp.net/api/v2/ticker/xrpusd/", "USD", "XRP"},
-		{"Bitstamp_LTC","https://www.bitstamp.net/api/v2/ticker/ltcusd/", "USD", "LTC"},
-		{"Bitstamp_ETH","https://www.bitstamp.net/api/v2/ticker/ethusd/", "USD", "ETH"},
-		{"Bitstamp_BCH","https://www.bitstamp.net/api/v2/ticker/bchusd/", "USD", "BCH"}}
-	var resseObjectCoinfloorAndBitstamp structs.CoinfloorTickerAndBitstamp
-	requestToExchange(resseObjectCoinfloorAndBitstamp, urlList, ch)
-
-
-	urlList2 := []Four{
-		{"IndependentReserve_BTC", "https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xbt&secondaryCurrencyCode=aud", "AUD","BTC"},
-		{"IndependentReserve_ETH","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=eth&secondaryCurrencyCode=aud", "AUD", "ETH"},
-		{"IndependentReserve_BCH","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=bch&secondaryCurrencyCode=aud", "AUD", "BCH"},
-		{"IndependentReserve_XRP","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xrp&secondaryCurrencyCode=aud", "AUD", "XRP"},
-		{"IndependentReserve_LTC","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=ltc&secondaryCurrencyCode=aud", "AUD", "LTC"}}
-	var responseObjectIndependentReserve structs.IndepentReserve
-	requestToExchange(responseObjectIndependentReserve, urlList2, ch)
-
-	urlList2a := []Four{
-		{"GEMINI_BTC", "https://api.gemini.com/v1/pubticker/btcusd", "USD", "BTC"}}
-	var responseObjectGeminiBTC structs.GeminiTickerBTC
-	requestToExchange(responseObjectGeminiBTC, urlList2a, ch)
-	urlList2b := []Four{
-		{"GEMINI_ETH", "https://api.gemini.com/v1/pubticker/ethusd", "USD", "ETH"}}
-	var responseObjectGeminiETH structs.GeminiTickerETH
-	requestToExchange(responseObjectGeminiETH, urlList2b, ch)
-
-	urlList3 := []Four{
-		{"BTCMarket_AUD_BTC", "https://api.btcmarkets.net/market/BTC/AUD/tick", "AUD", "BTC"},
-		{"BTCMarket_AUD_ETH", "https://api.btcmarkets.net/market/ETH/AUD/tick", "AUD", "ETH"},
-		{"BTCMarket_AUD_BCH","https://api.btcmarkets.net/market/BCHABC/AUD/tick", "AUD", "BCH"},
-		{"BTCMarket_AUD_XRP", "https://api.btcmarkets.net/market/XRP/AUD/tick", "AUD", "XRP"},
-		{"BTCMarket_AUD_LTC","https://api.btcmarkets.net/market/LTC/AUD/tick", "AUD", "LTC"}}
-	var responseObjectBTC structs.BTCMarket
-	requestToExchange(responseObjectBTC, urlList3, ch)
-
-	urlList4 := []Four{
-		{"ACX_AUD_BTC", "https://acx.io:443/api/v2/tickers/btcaud.json", "AUD", "BTC"},
-		{"ACX_AUD_ETH", "https://acx.io:443/api/v2/tickers/ethaud.json", "AUD", "ETH"},
-		{"ACX_AUD_BCH","https://acx.io:443/api/v2/tickers/bchaud.json", "AUD", "BCH"},
-		{"ACX_AUD_XRP", "https://acx.io:443/api/v2/tickers/ltcaud.json", "AUD", "XRP"},
-		{"ACX_AUD_LTC","https://acx.io:443/api/v2/tickers/xrpaud.json","AUD", "LTC"}}
-	var responseObjectACX structs.ACXTicker
-	requestToExchange(responseObjectACX, urlList4, ch)
-
-
-	urlList5 := []Four{
-		{"Coinjar_AUD_BTC", "https://data.exchange.coinjar.com/products/BTCAUD/ticker", "AUD", "BTC"},
-		{"Coinjar_AUD_ETH", "https://data.exchange.coinjar.com/products/ETHAUD/ticker", "AUD", "ETH"},
-		{"Coinjar_AUD_XRP","https://data.exchange.coinjar.com/products/XRPAUD/ticker","AUD", "XRP"},
-		{"Coinjar_AUD_LTC", "https://data.exchange.coinjar.com/products/LTCAUD/ticker", "AUD", "LTC"}}
-	var responseObjectCoinjar structs.Coinjar
-	requestToExchange(responseObjectCoinjar, urlList5, ch)
-	*/
-
-	for _, elem := range foo {
+	startData := getStartData()
+	for _, elem := range startData {
 		calculate(elem, ch)
 	}
 
@@ -254,63 +158,20 @@ func main() {
 		//log.Println(val1.rates["GBP2AUD"])
 	}
 
-	for _, app := range foo {
+	for _, app := range startData {
 		for range app.list {
 			val:=<-ch
 			if val.Error != nil {
-				log.Println(val.Error)
+				log.Println("Name:", val.Name, "Error", val.Error)
 			} else {
 				log.Println(val)
 				//val.Coin.LowFloat()
+				// TODO convert all to AUD
+				// standardise logging
 			}
 		}
 	}
-	/*
-	for range urlList {
-		// Use the response (<-ch).body
-		val:=<-ch
-		if val.Error != nil {
-			log.Println(val.Error)
-		} else {
-			log.Println(val.Currency)
-			val.Coin.LowFloat()
-		}
-		//log.Println(<-ch)
-		//bid() string
-		//ask()
-	}
 
-	for range urlList2 {
-		// Use the response (<-ch).body
-		log.Println(<-ch)
-	}
-
-	for range urlList2a {
-		// Use the response (<-ch).body
-		log.Println(<-ch)
-	}
-
-	for range urlList2b {
-		// Use the response (<-ch).body
-		log.Println(<-ch)
-	}
-
-	for range urlList3 {
-		// Use the response (<-ch).body
-		log.Println(<-ch)
-	}
-
-	for range urlList4 {
-		// Use the response (<-ch).body
-		log.Println(<-ch)
-	}
-
-	for range urlList5 {
-		// Use the response (<-ch).body
-		foo:=<-ch
-		log.Println(foo.Coin.VolumeFloat())
-	}*/
-	//val:= <- chRates
 	/*if DEBUG {
 		log.Println("got values from Crypto exchange")
 		log.Println(groupList)
@@ -322,4 +183,46 @@ func main() {
 
 
 	// send email if arbitage found
+}
+
+
+
+
+func getStartData() []startData {
+	return []startData{{"CoinfloorTickerAndBitstamp", []Four{
+		{"Coinfloor_BTC", "https://webapi.coinfloor.co.uk:8090/bist/XBT/GBP/ticker/", "GBP", "BTC"},
+		{"Coinfloor_ETH","https://webapi.coinfloor.co.uk:8090/bist/ETH/GBP/ticker/", "GBP", "ETH"},
+		{"Coinfloor_BCH","https://webapi.coinfloor.co.uk:8090/bist/BCH/GBP/ticker/", "GBP", "BCH"},
+		{"Bitstamp_BTC","https://www.bitstamp.net/api/v2/ticker/btcusd/", "USD", "BTC"},
+		{"Bitstamp_XRP","https://www.bitstamp.net/api/v2/ticker/xrpusd/", "USD", "XRP"},
+		{"Bitstamp_LTC","https://www.bitstamp.net/api/v2/ticker/ltcusd/", "USD", "LTC"},
+		{"Bitstamp_ETH","https://www.bitstamp.net/api/v2/ticker/ethusd/", "USD", "ETH"},
+		{"Bitstamp_BCH","https://www.bitstamp.net/api/v2/ticker/bchusd/", "USD", "BCH"}}},
+		startData{"IndepentReserve",[]Four{
+			{"IndependentReserve_BTC", "https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xbt&secondaryCurrencyCode=aud", "AUD","BTC"},
+			{"IndependentReserve_ETH","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=eth&secondaryCurrencyCode=aud", "AUD", "ETH"},
+			{"IndependentReserve_BCH","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=bch&secondaryCurrencyCode=aud", "AUD", "BCH"},
+			{"IndependentReserve_XRP","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xrp&secondaryCurrencyCode=aud", "AUD", "XRP"},
+			{"IndependentReserve_LTC","https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=ltc&secondaryCurrencyCode=aud", "AUD", "LTC"}}},
+		startData{"GeminiTickerBTC", []Four{
+			{"GEMINI_BTC", "https://api.gemini.com/v1/pubticker/btcusd", "USD", "BTC"}}},
+		startData{"GeminiTickerETH",[]Four{
+			{"GEMINI_ETH", "https://api.gemini.com/v1/pubticker/ethusd", "USD", "ETH"}}},
+		startData{"BTCMarket", []Four{
+			{"BTCMarket_AUD_BTC", "https://api.btcmarkets.net/market/BTC/AUD/tick", "AUD", "BTC"},
+			{"BTCMarket_AUD_ETH", "https://api.btcmarkets.net/market/ETH/AUD/tick", "AUD", "ETH"},
+			{"BTCMarket_AUD_BCH","https://api.btcmarkets.net/market/BCHABC/AUD/tick", "AUD", "BCH"},
+			{"BTCMarket_AUD_XRP", "https://api.btcmarkets.net/market/XRP/AUD/tick", "AUD", "XRP"},
+			{"BTCMarket_AUD_LTC","https://api.btcmarkets.net/market/LTC/AUD/tick", "AUD", "LTC"}}},
+		startData{"ACXTicker",[]Four{
+			{"ACX_AUD_BTC", "https://acx.io:443/api/v2/tickers/btcaud.json", "AUD", "BTC"},
+			{"ACX_AUD_ETH", "https://acx.io:443/api/v2/tickers/ethaud.json", "AUD", "ETH"},
+			{"ACX_AUD_BCH","https://acx.io:443/api/v2/tickers/bchaud.json", "AUD", "BCH"},
+			{"ACX_AUD_XRP", "https://acx.io:443/api/v2/tickers/ltcaud.json", "AUD", "XRP"},
+			{"ACX_AUD_LTC","https://acx.io:443/api/v2/tickers/xrpaud.json","AUD", "LTC"}}},
+		startData{"Coinjar",[]Four{
+			{"Coinjar_AUD_BTC", "https://data.exchange.coinjar.com/products/BTCAUD/ticker", "AUD", "BTC"},
+			{"Coinjar_AUD_ETH", "https://data.exchange.coinjar.com/products/ETHAUD/ticker", "AUD", "ETH"},
+			{"Coinjar_AUD_XRP","https://data.exchange.coinjar.com/products/XRPAUD/ticker","AUD", "XRP"},
+			{"Coinjar_AUD_LTC", "https://data.exchange.coinjar.com/products/LTCAUD/ticker", "AUD", "LTC"}}}}
 }
